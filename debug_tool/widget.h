0c0d48af9c6e959e4ed8f10d22553eab2d875411
HuaCheng Ma <3576527475a@gmail.com>
2025-01-27 21:51:37 +0800

    ♿️ 更新GUI组成

---
+++ debug_tool/widget.h
@@ -0,0 +1,18 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include "UI.h"
+#include <QApplication>
+#include <QWidget>
+
+class Widget : public QWidget {
+    Q_OBJECT
+    public:
+        Widget(QWidget *parent = nullptr);
+        ~Widget();
+    private:
+        Ui_Widget ui;  
+};
+
+
+#endif // WIDGET_H
